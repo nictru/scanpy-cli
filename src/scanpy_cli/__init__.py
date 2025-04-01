f938c2fd0d6d520a305d841ee35782392a0a7ac6
nictru <nictru32@gmail.com>
2025-04-01 10:36:55 +0200

    Add read_10x_h5 function

--- src/scanpy_cli/__init__.py
+++ src/scanpy_cli/__init__.py
@@ -3,6 +3,7 @@ import importlib.metadata
 from scanpy_cli.pp import pp
 from scanpy_cli.tl import tl
 from scanpy_cli.pl import pl
+from scanpy_cli.io import io
 
 
 @click.group()
@@ -17,6 +18,7 @@ def cli():
 cli.add_command(pp)
 cli.add_command(tl)
 cli.add_command(pl)
+cli.add_command(io)
 
 
 def main():
